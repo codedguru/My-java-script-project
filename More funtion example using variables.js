c2a2d37b5d438385040f596e4e1bab7e21015537
codedguru <davidadama2016@gmail.com>
2022-11-04 17:24:12 +0100

    Using Functions in Javascript

---
+++ More funtion example using variables.js
@@ -0,0 +1,9 @@
+function fourNums(){
+    var a = 200
+    var b = 300
+    var c = 400
+    var d = 500
+    var e = a + b + c + d 
+    console.log(e)
+}
+fourNums();
---
+++ Taking examples of functions.js
@@ -0,0 +1,5 @@
+//Lets take some examples of functions 
+function Hello(){
+    console.log("Hello world");
+}
+Hello ();
